322f098c5ffcd677479e905c20b24fad3555f7d6
thewangzl <thewangzl@gmail.com>
2025-03-18 20:20:43 +0800

    修改ndk版本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.example.suduko"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
